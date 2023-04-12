8cc531cf6bef73019a3931587e46c7f2520236fb
Travis Alstrand <travisalstrand@gmail.com>
2023-04-12 12:16:41 -0700

    backups working

--- modules/db_to_csv.py
+++ modules/db_to_csv.py
@@ -12,5 +12,27 @@ def backup_to_csv():
             brands_writer.writerow({
                 "brand_name": brand.brand_name
             })
-    print("\nBackup created!")
+
+    with open("./csvs/backup_inventory.csv", "w") as inv_csvfile:
+        inv_fieldnames = ["product_name", "product_price", "product_quantity",
+                          "date_updated", "brand_name"]
+        inv_writer = csv.DictWriter(inv_csvfile, fieldnames=inv_fieldnames)
+        inv_writer.writeheader()
+        for product in session.query(Product):
+            unclean_price = float(product.product_price / 100)
+            unclean_date = product.date_updated.strftime("%m/%d/%Y")
+            if unclean_date[3] == "0":
+                unclean_date = unclean_date[:3] + unclean_date[4:]
+            if unclean_date[0] =="0":
+                unclean_date = unclean_date[1:]
+            brand_to_add = session.query(Brand). \
+                filter(Brand.brand_id == product.brand_id).first().brand_name
+            inv_writer.writerow({
+                "product_name": product.product_name,
+                "product_price": "${0:.2f}".format(unclean_price),
+                "product_quantity": product.product_quantity,
+                "date_updated": unclean_date,
+                "brand_name": brand_to_add
+            })
+    print("\nBackups created!")
     time.sleep(2)
